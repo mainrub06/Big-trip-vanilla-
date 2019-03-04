f0ea0e63b739200efb75a1c3848c319c81474612
Vladislav <mainrub06@gmail.com>
2019-03-04 19:07:19 +0300

    eslint fix

--- src/main.js
+++ src/main.js
@@ -5,73 +5,73 @@ const filtersBlock = document.querySelector(`.trip-filter`);
 const pointsBlock = document.querySelector(`.trip-day__items`);
 
 const FILTERS_ARRAY = [{
-    name: `everything`,
-    check: true
-  },
-  {
-    name: `future`
-  },
-  {
-    name: `past`
-  }
+  name: `everything`,
+  check: true
+},
+{
+  name: `future`
+},
+{
+  name: `past`
+}
 ];
 
 const POINT_ARRAY = [{
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  },
-  {
-    icon: `🚕`,
-    title: `Check into a hotel`,
-    start: `10:00`,
-    end: `11:00`,
-    hours: `1h 30m`,
-    price: `20`
-  }
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+},
+{
+  icon: `🚕`,
+  title: `Check into a hotel`,
+  start: `10:00`,
+  end: `11:00`,
+  hours: `1h 30m`,
+  price: `20`
+}
 ];
 
 const renderFilters = function (element) {
@@ -97,24 +97,24 @@ const removeFilters = () => {
 
 const renderRandomPoints = (num, array) => {
   let random = (min, max) => {
-    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
     rand = Math.round(rand);
     return rand;
-  }
+  };
   removeFilters();
   for (let i = 0; i < random(0, num); i++) {
     renderPoints(array[i]);
   }
-}
+};
 
 filtersBlock.addEventListener(`click`, () => {
   renderRandomPoints(7, POINT_ARRAY);
 });
 
-FILTERS_ARRAY.forEach(item => {
+FILTERS_ARRAY.forEach((item) => {
   renderFilters(item);
 });
 
-POINT_ARRAY.forEach(item => {
+POINT_ARRAY.forEach((item) => {
   renderPoints(item);
 });
