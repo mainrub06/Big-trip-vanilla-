b666373818695e4644463e7d180546d7d593f833
Владислав Коломийцев <mainrub@MacBook-Pro-Vladislav.local>
2021-01-12 21:03:50 +0300

    Fix: фикс времени и рендера

--- src/components/TripDay.js
+++ src/components/TripDay.js
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { Component } from "../core/Component";
 import EventItem from "./EventItem";
 
@@ -11,7 +12,7 @@ export default class TravelDay extends Component {
   renderPoints() {
     const pointsData = this._data.points;
     const $items = this._element.querySelector(`.trip-day__items`);
-
+    
     this._points = pointsData.map((data) => {
       const eventItem = new EventItem(data);
 
@@ -40,17 +41,13 @@ export default class TravelDay extends Component {
   }
 
   get template() {
-    const date = new Date(this._data.timestamp);
-    const day = date.getDate();
-    const monthAndYear = date.toLocaleDateString(`en`, {
-      formatMatcher: `month year`,
-    });
+    const date = new Date(Number(this._data.timestamp));
 
     return `<section class="trip-day">
               <article class="trip-day__info">
                 <span class="trip-day__caption">Day</span>
-                <p class="trip-day__number">${day}</p>
-                <h2 class="trip-day__title">${monthAndYear}</h2>
+                <p class="trip-day__number">${format(date, 'dd')}</p>
+                <h2 class="trip-day__title">${format(date, 'MMMMM yyyy')}</h2>
               </article>
               <div class="trip-day__items"></div>
             </section>`.trim();
